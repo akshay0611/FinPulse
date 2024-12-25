35a2bc617e309ebf5af73130ff67b67aae7947d6
Akshay <akshay.allen26200@gmail.com>
2024-12-25 21:04:05 +0530

    Removed slide

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-    include: ['react-scroll'], // Ensure react-scroll is included in dependencies optimization
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-scroll'], // Treat react-scroll as an external dependency if needed
-    },
   },
   base: '/', // Adjust this if your project is served from a sub-path
 });
